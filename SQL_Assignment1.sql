c3342d4f8984419882cccd9ce21d57c06aee23c0
irfandumangit config --global user.name irfandumangit config --global user.mail :irfanduman@gmail.com <115734633+irfanduman@users.noreply.github.com>
2023-01-13 09:35:03 +0000

    Update SQL_Assignment1.sql

--- SQL_Assignment1.sql
+++ SQL_Assignment1.sql
@@ -1,4 +1,5 @@
 USE SampleRetail
+
 /* 1. How many customers are in each city? Your solution should include the city name and the number of customers sorted from highest to lowest.*/
 SELECT city, COUNT(customer_id) AS number_of_customers
 FROM [sale].[customer]
@@ -22,14 +23,14 @@ GROUP BY customer_id
 SELECT order_id, SUM(quantity*list_price*discount) AS total_amount
 FROM sale.order_item
 GROUP BY order_id
-ORDER by oder DESC;
+ORDER by total_amount DESC;
 
 
 --- 5. Find the order id that has the maximum average product price. Your solution should include only one row with the order id and average product price. ---
-SELECT TOP 1 order_id, AVG(list_price) As Average
+SELECT TOP 1 order_id, AVG(list_price) As average_price
 FROM sale.order_item
 GROUP BY order_id
-ORDER by Average DESC;
+ORDER by average_price DESC;
 
 
 
@@ -37,6 +38,7 @@ ORDER by Average DESC;
 SELECT brand_id, product_id, list_price
 FROM product.product
 ORDER BY brand_id, list_price DESC;
+
 
 --- 7. Write a query that displays brand_id, product_id and list_price, but this time sorted first by list_price (in descending order), and then by brand_id (in ascending order). ---
 SELECT brand_id, product_id, list_price
@@ -51,28 +53,29 @@ ORDER BY list_price DESC, brand_id;
 
 
 --- 9. Write a query to pull the first 10 rows and all columns from the product table that have a list_price greater than or equal to 3000. ---
-
-
+SELECT TOP 10 *
+FROM product.product
+WHERE list_price >= 3000
 
 
 
 
 --- 10. Write a query to pull the first 5 rows and all columns from the product table that have a list_price less than 3000. ---
-
-
-
+SELECT TOP 5 *
+FROM product.product
+WHERE list_price < 3000
 
 
 
 --- 11. Find all customer last names that start with 'B' and end with 's'. ---
-
-
-
-
+SELECT DISTINCT last_name
+FROM sale.customer
+WHERE last_name LIKE 'B%s'
 
 
 --- 12. Use the customer table to find all information regarding customers whose address is Allen or Buffalo or Boston or Berkeley. ---
-
-
+SELECT *
+FROM sale.customer
+WHERE city IN ('Allen', 'Buffalo', 'Boston', 'Berkeley')
 
 
